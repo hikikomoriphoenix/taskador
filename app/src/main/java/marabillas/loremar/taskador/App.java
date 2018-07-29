241315e7055f7cd3bd05f11870a14256c14a8b4e
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-30 03:00:35 +0800

    Create App class extending Application class.

---
+++ app/src/main/java/marabillas/loremar/taskador/App.java
@@ -0,0 +1,9 @@
+package marabillas.loremar.taskador;
+
+import android.app.Application;
+
+/**
+ * This is the core encompassing everything in taskador. When destroyed, taskador is destroyed.
+ */
+public class App extends Application {
+}
