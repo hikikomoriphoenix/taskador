f8555790164905706e7871ca06d93c5f3652ffe8
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-31 02:41:38 +0800

    Create AuthenticatorService.

---
+++ app/src/main/java/marabillas/loremar/taskador/account/AuthenticatorService.java
@@ -0,0 +1,24 @@
+package marabillas.loremar.taskador.account;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Service required for AccountAuthenticator to be bound with
+ */
+public class AuthenticatorService extends Service {
+    private AccountAuthenticator authenticator;
+
+    @Override
+    public void onCreate() {
+        authenticator = new AccountAuthenticator(this);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return authenticator.getIBinder();
+    }
+}
