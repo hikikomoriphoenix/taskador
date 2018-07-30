b242a9d25b96fd88ff6a1ff1ac255a0ed38a42f4
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-30 10:25:49 +0800

    Create SplashActivity.

---
+++ app/src/main/java/marabillas/loremar/taskador/ui/activity/SplashActivity.java
@@ -0,0 +1,10 @@
+package marabillas.loremar.taskador.ui.activity;
+
+import android.app.Activity;
+
+/**
+ * This is taskador's main launcher activity. It displays a splash screen while taskador is
+ * logging in to the server or making transitions from one screen to another.
+ */
+public class SplashActivity extends Activity {
+}
