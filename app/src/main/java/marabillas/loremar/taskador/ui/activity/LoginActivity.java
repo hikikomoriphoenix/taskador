66173a1139f124151a8c8480828f4918a5ec3234
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-30 10:08:30 +0800

    Create LoginActivity.

---
+++ app/src/main/java/marabillas/loremar/taskador/ui/activity/LoginActivity.java
@@ -0,0 +1,12 @@
+package marabillas.loremar.taskador.ui.activity;
+
+import android.app.Activity;
+
+/**
+ * This activity facilitates the Login screen. The screen is shown to the user when taskador can
+ * not authenticate the user. The user have to log in with his/her username and password. The
+ * server then returns an auth token which will be used for the user's account authentication.
+ * The screen also provides sign up option if he/she doesn't have an account yet.
+ */
+public class LoginActivity extends Activity {
+}
