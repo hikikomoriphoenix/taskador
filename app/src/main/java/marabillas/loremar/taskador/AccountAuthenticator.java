26ae7ffc75e173ad635e9f9ab2aa077951dcea57
hikikomoriphoenix <dondon1987@yandex.com>
2018-07-30 11:21:25 +0800

    Implement addAccount for AccountAuthenticator.

--- app/src/main/java/marabillas/loremar/taskador/AccountAuthenticator.java
+++ app/src/main/java/marabillas/loremar/taskador/AccountAuthenticator.java
@@ -3,13 +3,18 @@ package marabillas.loremar.taskador;
 import android.accounts.AbstractAccountAuthenticator;
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorResponse;
-import android.accounts.NetworkErrorException;
+import android.accounts.AccountManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import marabillas.loremar.taskador.ui.activity.LoginActivity;
+
 public class AccountAuthenticator extends AbstractAccountAuthenticator {
+    private Context context;
     public AccountAuthenticator(Context context) {
         super(context);
+        this.context = context;
     }
 
     @Override
@@ -18,17 +23,29 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator {
     }
 
     @Override
-    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType, String[] requiredFeatures, Bundle options) throws NetworkErrorException {
+    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType, String authTokenType, String[] requiredFeatures, Bundle options) {
+        // Create and return a KEY_INTENT that specifies that the user should be directed to the
+        // Login screen when the user chooses to add an account from the device's account settings.
+
+        final Intent intent = new Intent(context, LoginActivity.class);
+
+        intent.putExtra(context.getPackageName(), accountType);
+        intent.putExtra("full_access", authTokenType);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+
+        final Bundle bundle = new Bundle();
+        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
+
+        return bundle;
+    }
+
+    @Override
+    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) {
         return null;
     }
 
     @Override
-    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account, Bundle options) throws NetworkErrorException {
-        return null;
-    }
-
-    @Override
-    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) {
         return null;
     }
 
@@ -38,12 +55,12 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator {
     }
 
     @Override
-    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
+    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) {
         return null;
     }
 
     @Override
-    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) throws NetworkErrorException {
+    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) {
         return null;
     }
 }
