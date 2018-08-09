939aa4d6a0cf198a3b1037426ba91b70ede2e070
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-09 15:51:18 +0800

    Fix SplashActivity not extending Activity.

--- app/src/main/java/marabillas/loremar/taskador/ui/activity/SplashActivity.java
+++ app/src/main/java/marabillas/loremar/taskador/ui/activity/SplashActivity.java
@@ -1,12 +1,14 @@
 package marabillas.loremar.taskador.ui.activity;
 
+import android.app.Activity;
+
 import marabillas.loremar.taskador.network.SignupTask;
 
 /**
  * This is taskador's main launcher activity. It displays a splash screen while taskador is
  * logging in to the server or making transitions from one screen to another.
  */
-public class SplashActivity implements SignupTask.ResultHandler {
+public class SplashActivity extends Activity implements SignupTask.ResultHandler {
     @Override
     public void newAccountSaved(String message) {
 
