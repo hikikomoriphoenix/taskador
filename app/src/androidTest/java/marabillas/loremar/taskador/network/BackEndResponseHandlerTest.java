c682cb4f89c1f942abdcd7f82715bd5eba01a4c5
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-19 02:18:04 +0800

    Fix the test for validateResponse.
    -Use a background thread for sending POST requests.
    -Change cookieHandle to cookieHandled.
    -Use Log to display response's status code and data.
    -Add informative comments.

--- app/src/androidTest/java/marabillas/loremar/taskador/network/BackEndResponseHandlerTest.java
+++ app/src/androidTest/java/marabillas/loremar/taskador/network/BackEndResponseHandlerTest.java
@@ -1,5 +1,8 @@
 package marabillas.loremar.taskador.network;
 
+import android.os.Handler;
+import android.os.HandlerThread;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -10,6 +13,7 @@ import java.util.concurrent.CountDownLatch;
 
 import marabillas.loremar.taskador.BuildConfig;
 
+import static marabillas.loremar.taskador.utils.LogUtils.log;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -24,42 +28,63 @@ public class BackEndResponseHandlerTest {
         final String url = BuildConfig.backend_url + "signup.php";
         final CountDownLatch pause = new CountDownLatch(1);
 
+        // Track the handling of shared-hosting site's cookie. When handled, try to check if the
+        // back end server will return a JSON response.
         CookieHandledTracker tracker = new CookieHandledTracker() {
-            private boolean cookieHandle;
+            private boolean cookieHandled;
 
             @Override
             public boolean isCookieHandled() {
-                return cookieHandle;
+                return cookieHandled;
             }
 
             @Override
             public void resetCookieHandledTracking() {
-                cookieHandle = false;
+                cookieHandled = false;
             }
 
             @Override
             public void finalizeCookieHandling() {
-                cookieHandle = true;
-                HttpClient client = new HttpClient();
-                HashMap<String, String> form = new HashMap<>();
-                form.put("username", "test1");
-                form.put("password", "password");
-                try {
-                    BackEndResponse response = client.postForm(form, url);
-                    assertThat(response.getContentType(), is("application/json"));
-                } catch (IOException e) {
-                    Assert.fail(e.getMessage());
-                } finally {
-                    pause.countDown();
-                }
+                // Sending POST request should be done on a background thread.
+                HandlerThread thread = new HandlerThread("network thread");
+                thread.start();
+                final Handler handler = new Handler(thread.getLooper());
+                handler.post(new Runnable() {
+                    public void run() {
+                        cookieHandled = true;
+                        HttpClient client = new HttpClient();
+                        HashMap<String, String> form = new HashMap<>();
+                        form.put("username", "test1");
+                        form.put("password", "password");
+                        try {
+                            BackEndResponse response = client.postForm(form, url);
+                            assertThat(response.getContentType(), is("application/json"));
+                            log("status: " + response.getStatusCode());
+                            log("data: " + response.getData());
+                        } catch (IOException e) {
+                            Assert.fail(e.getMessage());
+                        } finally {
+                            // Unpause the waiting thread, thus, proceeding to the end of this test.
+                            pause.countDown();
+                            // End this background thread.
+                            handler.getLooper().quitSafely();
+                        }
+                    }
+                });
             }
         };
 
-        BackEndResponseHandler handler = new BackEndResponseHandler(tracker);
+        // Set content-type to 'text/html' before calling validateResponse. This  will trigger
+        // the handling of shared-hosting site's cookie.
+        BackEndResponseHandler responseHandler = new BackEndResponseHandler(tracker);
         BackEndResponse response = new BackEndResponse();
         response.setContentType("text/html");
-        boolean valid = handler.validateResponse(response, url);
+
+        boolean valid = responseHandler.validateResponse(response, url);
+
         assertThat(valid, is(false));
+
+        // This thread needs to wait for further results
         try {
             pause.await();
         } catch (InterruptedException e) {
