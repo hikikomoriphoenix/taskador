c15286f5e185fc27b47ff4752cc03d34d60e7e97
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-21 16:39:48 +0800

    Add account with auth token to Account Manager if signup is successful.

--- app/src/main/java/marabillas/loremar/taskador/network/tasks/SignupTask.java
+++ app/src/main/java/marabillas/loremar/taskador/network/tasks/SignupTask.java
@@ -3,11 +3,16 @@ package marabillas.loremar.taskador.network.tasks;
 import java.io.IOException;
 import java.util.HashMap;
 
+import marabillas.loremar.taskador.App;
 import marabillas.loremar.taskador.BuildConfig;
+import marabillas.loremar.taskador.json.FailedToGetFieldException;
 import marabillas.loremar.taskador.json.JSON;
 import marabillas.loremar.taskador.network.BackEndAPICallTasker;
 import marabillas.loremar.taskador.network.BackEndResponse;
 import marabillas.loremar.taskador.network.HttpClient;
+
+import static marabillas.loremar.taskador.utils.AccountUtils.createAccount;
+import static marabillas.loremar.taskador.utils.LogUtils.logError;
 
 public class SignupTask extends RunnableTask<SignupTask.ResultHandler> {
     private String username;
@@ -44,6 +49,13 @@ public class SignupTask extends RunnableTask<SignupTask.ResultHandler> {
 
     @Override
     public void onStatusOK(String message, JSON data) {
+        try {
+            String token = data.getString("token");
+            createAccount(App.getInstance().getApplicationContext(), username, password, token);
+        } catch (FailedToGetFieldException e) {
+            logError("Couldn't add account to Account Manager. No available auth token to set: ");
+        }
+
         ResultHandler resultHandler = getResultHandler();
         if (resultHandler != null) {
             resultHandler.newAccountSaved(message);
