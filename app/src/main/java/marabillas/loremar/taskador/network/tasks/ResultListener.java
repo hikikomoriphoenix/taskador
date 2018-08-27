952acc94aed0fbaa72df6a5acc53ea13d1edaade
hikikomoriphoenix <dondon1987@yandex.com>
2018-08-28 03:42:22 +0800

    Add javadocs to ResultListener interface

--- app/src/main/java/marabillas/loremar/taskador/network/tasks/ResultListener.java
+++ app/src/main/java/marabillas/loremar/taskador/network/tasks/ResultListener.java
@@ -2,10 +2,24 @@ package marabillas.loremar.taskador.network.tasks;
 
 import marabillas.loremar.taskador.json.JSON;
 
+/**
+ * A ResultListener is notified when a response has been received from the back-server and
+ * validated by a BackEndResponseHandler, by calling one of the callback methods corresponding to
+ * the status of the response.
+ */
 public interface ResultListener {
+    /**
+     * Callback method for response with 200 OK status.
+     */
     void onStatusOK(String message, JSON data);
 
+    /**
+     * Callback method for response with 400 or 422 status
+     */
     void onClientError(String message);
 
+    /**
+     * Callback method for response with 500 INTERNAL SERVER ERROR status
+     */
     void onServerError(String message);
 }
