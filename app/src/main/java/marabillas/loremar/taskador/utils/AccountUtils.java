e7cd6ccddfb3664dbecc0a46fa376b3143d7d219
hikikomoriphoenix <dondon1987@yandex.com>
2018-09-15 09:38:15 +0800

    Introduce utility method for getting auth token from given username

--- app/src/main/java/marabillas/loremar/taskador/utils/AccountUtils.java
+++ app/src/main/java/marabillas/loremar/taskador/utils/AccountUtils.java
@@ -2,7 +2,15 @@ package marabillas.loremar.taskador.utils;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.content.Context;
+
+import junit.framework.Assert;
+
+import java.io.IOException;
+
+import marabillas.loremar.taskador.App;
 
 /**
  * Account-related helper methods
@@ -23,5 +31,31 @@ public final class AccountUtils {
         AccountManager am = AccountManager.get(context);
         am.addAccountExplicitly(account, password, null);
         am.setAuthToken(account, "full_access", authToken);
+    }
+
+    /**
+     * Get stored auth token for an account with the given username.
+     *
+     * @param username account username
+     * @return auth token as a string
+     */
+    public static String getAuthToken(String username) {
+        Context context = App.getInstance().getApplicationContext();
+
+        AccountManager am = AccountManager.get(context);
+        Account account = new Account(username, context.getPackageName());
+
+        String token = null;
+        try {
+            token = am.blockingGetAuthToken(account, "full_access", true);
+        } catch (OperationCanceledException e) {
+            Assert.fail(e.getMessage());
+        } catch (IOException e) {
+            Assert.fail(e.getMessage());
+        } catch (AuthenticatorException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        return token;
     }
 }
