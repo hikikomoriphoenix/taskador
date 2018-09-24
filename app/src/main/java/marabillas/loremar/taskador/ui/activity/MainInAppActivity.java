fc0356a53c4a49e6b428b08a7d99efbcc7d56d01
hikikomoriphoenix <dondon1987@yandex.com>
2018-09-24 10:43:45 +0800

    Prepare main in-app screen with empty layout, theme, and testing

---
+++ app/src/main/java/marabillas/loremar/taskador/ui/activity/MainInAppActivity.java
@@ -0,0 +1,15 @@
+package marabillas.loremar.taskador.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import marabillas.loremar.taskador.R;
+
+public class MainInAppActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_inapp);
+    }
+}
