31c78ee90c3f0dec1c30f958a48b0b03cd082d54
hikikomoriphoenix <dondon1987@yandex.com>
2018-10-01 23:40:38 +0800

    Unfocus EditText when soft keyboard closes

--- app/src/main/java/marabillas/loremar/taskador/ui/activity/MainInAppActivity.java
+++ app/src/main/java/marabillas/loremar/taskador/ui/activity/MainInAppActivity.java
@@ -1,11 +1,14 @@
 package marabillas.loremar.taskador.ui.activity;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 import marabillas.loremar.taskador.R;
 import marabillas.loremar.taskador.components.MainInAppEventHandler;
@@ -19,12 +22,14 @@ import marabillas.loremar.taskador.ui.fragment.TopWordsFragment;
  * finished tasks and also features listing of most frequently used words for tasks. These
  * features are contained in a ViewPager allowing the user to swipe between them.
  */
-public class MainInAppActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+public class MainInAppActivity extends AppCompatActivity implements ViewPager
+        .OnPageChangeListener, ViewTreeObserver.OnGlobalLayoutListener {
     private ToDoTasksFragment toDoTasksFragment;
     private FinishedTasksFragment finishedTasksFragment;
     private TopWordsFragment topWordsFragment;
     private ViewPager pager;
     private MainInAppEventHandler mainInAppEventHandler;
+    private View contentView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,6 +47,9 @@ public class MainInAppActivity extends AppCompatActivity implements ViewPager.On
         MainInappViewPagerAdapter adapter = new MainInappViewPagerAdapter(fm, fragments);
         pager.setAdapter(adapter);
         pager.addOnPageChangeListener(this);
+
+        contentView = findViewById(android.R.id.content);
+        contentView.getViewTreeObserver().addOnGlobalLayoutListener(this);
     }
 
     @Override
@@ -70,5 +78,22 @@ public class MainInAppActivity extends AppCompatActivity implements ViewPager.On
     public void setMainInAppEventHandler(MainInAppEventHandler mainInAppEventHandler) {
         this.mainInAppEventHandler = mainInAppEventHandler;
         this.mainInAppEventHandler.onMainInAppIsReady(this);
+    }
+
+
+    @Override
+    public void onGlobalLayout() {
+        // Try to remove focus from EditText when soft keyboard is closed.
+        // When soft keyboard closes the height of visible portion of the screen becomes equal to
+        // the height of the total screen. In other words, the screen becomes completely visible.
+        Rect visibleRect = new Rect();
+        contentView.getWindowVisibleDisplayFrame(visibleRect);
+        int totalHeight = contentView.getRootView().getHeight();
+        int visibleHeight = visibleRect.bottom;
+        if (visibleHeight == totalHeight) {
+            if (getCurrentFocus() != null) {
+                getCurrentFocus().clearFocus();
+            }
+        }
     }
 }
