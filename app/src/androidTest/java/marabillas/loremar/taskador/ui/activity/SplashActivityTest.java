a6fecbaff669c40300dd2049e401e7cffbcf235a
hikikomoriphoenix <dondon1987@yandex.com>
2018-10-25 10:26:21 +0800

    Add tests for login task in splash screen

--- app/src/androidTest/java/marabillas/loremar/taskador/ui/activity/SplashActivityTest.java
+++ app/src/androidTest/java/marabillas/loremar/taskador/ui/activity/SplashActivityTest.java
@@ -1,5 +1,7 @@
 package marabillas.loremar.taskador.ui.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.test.rule.ActivityTestRule;
 
 import junit.framework.Assert;
@@ -10,16 +12,50 @@ import org.junit.Test;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import marabillas.loremar.taskador.App;
+import marabillas.loremar.taskador.background.SplashBackgroundTasker;
+
 public class SplashActivityTest {
     @Rule
-    public ActivityTestRule<SplashActivity> activityTestRule = new ActivityTestRule<>
-            (SplashActivity.class);
+    public ActivityTestRule<SplashActivity> activityTestRule;
 
     @Test
-    public void test() {
+    public void testLogin() {
+        login("test1", "password");
+        await();
+    }
+
+    @Test
+    public void testLoginWrongUsername() {
+        login("userunknown", "password");
+        await();
+    }
+
+    @Test
+    public void testLoginWrongPassword() {
+        login("test1", "passport");
+        await();
+    }
+
+    private void login(String username, String password) {
+        Intent intent = new Intent(App.getInstance(), SplashActivity.class);
+
+        Bundle input = new Bundle();
+        input.putInt("action", SplashBackgroundTasker.Action.LOGIN.ordinal());
+        input.putString("username", username);
+        input.putString("password", password);
+        intent.putExtra("input", input);
+
+        App.getInstance().setBackgroundTaskManagerSupport(true);
+
+        activityTestRule = new ActivityTestRule<>(SplashActivity.class);
+        activityTestRule.launchActivity(intent);
+    }
+
+    private void await() {
         CountDownLatch latch = new CountDownLatch(1);
         try {
-            latch.await(10, TimeUnit.SECONDS);
+            latch.await(1, TimeUnit.MINUTES);
         } catch (InterruptedException e) {
             Assert.fail(e.getMessage());
         }
